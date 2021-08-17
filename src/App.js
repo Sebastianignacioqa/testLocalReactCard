fe61e4aa6876422ab92a345c299dc20bb604baa6
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-16 21:29:17 -0400

    Prueba react desde local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div> <Card imagen1="https://picsum.photos/id/237/200/300"/>
+  <Card imagen1="https://picsum.photos/id/237/200/300" /></div>)
 }
 
 export default App;
